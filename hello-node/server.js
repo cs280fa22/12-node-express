a1cac6511d749d20382297767d12487c95a7edfa
Ali Madooei <alimadooei@gmail.com>
2022-11-05 19:30:58 -0400

    Step 1: Node

---
+++ hello-node/server.js
@@ -0,0 +1,8 @@
+const fs = require("fs");     // import File System module
+const http = require("http"); // import HTTP Networking module
+
+console.log("Hello Node!");
+// console.log(window);    // not available in Node
+// console.log(document);  // not available in Node
+console.log(fs);
+console.log(http);
