0c942199856e5e009a7f5030ca93f3b5ffd56769
Ali Madooei <alimadooei@gmail.com>
2022-11-06 07:44:59 -0500

    Step 9: CI/CD

--- hello-api/server.js
+++ hello-api/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { schools, terms, courses } from "./data.js";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
